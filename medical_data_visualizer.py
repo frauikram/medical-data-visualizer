7b92e6761e2dfe4f1afdf8bba7edd141b98e500b
frauikram <119944932+frauikram@users.noreply.github.com>
2023-11-15 09:29:51 +0100

    Normalize data

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -10,7 +10,7 @@ df = pd.read_csv('medical_examination.csv')
 df['overweight'] = (df['weight']/((df['height']/100)**2) > 25).astype(int)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
-
+df[['gluc','cholesterol']] = (df[['gluc','cholesterol']] > 1).astype(int)
 
 # Draw Categorical Plot
 def draw_cat_plot():
